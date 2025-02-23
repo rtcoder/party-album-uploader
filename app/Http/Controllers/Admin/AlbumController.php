22bbb5ba6832262f9373ba288549f48e189e77a0
Dawid Jeż <dawidjez@gmail.com>
2025-02-23 13:34:31 +0100

    Add layout components and update package-lock.json
    Introduced `AppLayout` and `GuestLayout` components to structure application layouts. Updated `package-lock.json` to include new dependencies for frontend development and build configurations. These changes enhance the codebase by improving maintainability and modernizing dependencies.

---
+++ app/Http/Controllers/Admin/AlbumController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+    public function index(): Factory|Application|View
+    {
+        $albums = Album::query()->latest()->get();
+        return view('admin.albums.index', compact('albums'));
+    }
+
+    public function create(): Factory|Application|View
+    {
+        return view('admin.albums.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Album::query()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('albums.index')->with('success', 'Album utworzony!');
+    }
+
+    public function show(Album $album): Factory|Application|View
+    {
+        return view('admin.albums.show', compact('album'));
+    }
+
+    public function edit(Album $album): Factory|Application|View
+    {
+        return view('admin.albums.edit', compact('album'));
+    }
+
+    public function update(Request $request, Album $album):RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $album->update([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('albums.index')->with('success', 'Album zaktualizowany!');
+    }
+
+    public function destroy(Album $album): RedirectResponse
+    {
+        $album->delete();
+        return redirect()->route('albums.index')->with('success', 'Album usunięty!');
+    }
+}
