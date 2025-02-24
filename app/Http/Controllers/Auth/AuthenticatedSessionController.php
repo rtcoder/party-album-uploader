d27953ceda05c3e220c3ad07ef61be93c2b1cc00
Dawid Jeż <dawidjez@gmail.com>
2025-02-24 21:50:39 +0100

    Remove controller docblocks and refine session redirect
    Eliminated redundant docblocks in authentication controllers, improving code readability and reducing clutter. Updated the redirect route in the `AuthenticatedSessionController::store` method to point to 'admin' for better alignment with application flow.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,20 +7,18 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
     public function create(): View
     {
         return view('auth.login');
     }
 
     /**
-     * Handle an incoming authentication request.
+     * @throws ValidationException
      */
     public function store(LoginRequest $request): RedirectResponse
     {
@@ -28,12 +26,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('admin', absolute: false));
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
+
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -11,16 +11,13 @@ use Illuminate\View\View;
 
 class ConfirmablePasswordController extends Controller
 {
-    /**
-     * Show the confirm password view.
-     */
     public function show(): View
     {
         return view('auth.confirm-password');
     }
 
     /**
-     * Confirm the user's password.
+     * @throws ValidationException
      */
     public function store(Request $request): RedirectResponse
     {
